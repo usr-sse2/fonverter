a360c0d97b94ddee0ee3b4ab4a8ed89f215063bc
Michael Belyaev <usrsse2@me.com>
2020-05-04 13:48:26 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using SharpFNT;
+
+namespace fonverter
+{
+    static class Program
+    {
+        public static int Main(string[] args)
+        {
+            static int ShowUsage()
+            {
+                Console.Error.WriteLine("Usage: fonverter a.ext1 b.ext2");
+                Console.Error.WriteLine("File extensions specify formats to convert from and to.");
+                Console.Error.WriteLine("Supported formats: 'bin' (binary), 'fnt' (text) and 'xml' (XML).");
+                return 1;
+            }
+
+            static FormatHint GetFormat(string filePath) => Path.GetExtension(filePath).ToLower() switch
+            {
+                ".fnt" => FormatHint.Text,
+                ".bin" => FormatHint.Binary,
+                ".xml" => FormatHint.XML,
+                _ => throw new NotSupportedException()
+            };
+
+            if (args.Length != 2)
+                return ShowUsage();
+
+            try
+            {
+                BitmapFont.FromFile(args[0], GetFormat(args[0])).Save(args[1], GetFormat(args[1]));
+            }
+            catch (NotSupportedException)
+            {
+                return ShowUsage();
+            }
+            return 0;
+        }
+    }
+}
